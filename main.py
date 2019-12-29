4d09971a98898bb7d6128b930132bfbccde6106b
Ivaylo Spasov <ivaylo.pif@gmail.com>
2019-12-29 12:48:30 +0200

    Промени за работа с чисти инвентарни номера, а не с уникални такива.

--- main.py
+++ main.py
@@ -10,13 +10,14 @@ while True:
     inventaren = input("Въведете инвентарен номер: ")
 
     df = pd.read_csv('cmol.csv')
-    inv_data = df[['indname', 'vidname', 'nameda', 'broi', 'molname']]
+    inv_data = df[['inv', 'vidname', 'nameda', 'broi', 'molname']]
     df_cols = ['номер', 'вид', 'име', 'брой', 'МОЛ']
     inv_data.columns = df_cols
     booleans = []
     for inv_number in inv_data['номер']:
-        inv_number = inv_number.strip()
-        if inv_number == str(inventaren):
+        #inv_number = inv_number.strip()
+        #if inv_number == str(inventaren):
+        if str(inv_number) == inventaren:
         # if int(inv_number) == int(inventaren):
              booleans.append(True)
         else:
