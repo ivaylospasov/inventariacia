60e07faa324d3bfac121689402baa3c6cf20d60e
Ivaylo Spasov <ivaylo.pif@gmail.com>
2019-12-29 11:05:26 +0200

    Add the main file

---
+++ main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import pandas as pd
+
+pd.set_option('display.max_rows', None)
+#pd.set_option('display.max_columns', None)
+#pd.set_option('display.width', None)
+pd.set_option('display.max_colwidth', -1)
+
+while True:
+    inventaren = input("Въведете инвентарен номер: ")
+
+    df = pd.read_csv('cmol.csv')
+    inv_data = df[['indname', 'vidname', 'nameda', 'broi', 'molname']]
+    df_cols = ['номер', 'вид', 'име', 'брой', 'МОЛ']
+    inv_data.columns = df_cols
+    booleans = []
+    for inv_number in inv_data['номер']:
+        inv_number = inv_number.strip()
+        if inv_number == str(inventaren):
+        # if int(inv_number) == int(inventaren):
+             booleans.append(True)
+        else:
+            booleans.append(False)
+    #is_long = inv_data['номер'] == str(inventaren)
+    is_long = pd.Series(booleans)
+    print(inv_data[is_long])
